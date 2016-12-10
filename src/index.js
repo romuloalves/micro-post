48cddbb2da1e382e972e15e9eb400fe708593a9e
Rômulo Alves <romuloaalv@gmail.com>
2016-12-10 00:06:06 -0200

    Including Accept Header even when the unaccepted method is called

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ const ALLOWED_HTTP_METHOD = 'POST'
 
 module.exports = exports = function (fn) {
   return (req, res) => {
+    res.setHeader('Access-Control-Request-Method', ALLOWED_HTTP_METHOD)
     const {method} = req
     if (method !== ALLOWED_HTTP_METHOD) {
       res.writeHead(405)
       res.end('Method Not Allowed')
       return
     }
-    res.setHeader('Access-Control-Request-Method', ALLOWED_HTTP_METHOD)
     return fn(req, res)
   }
 }
