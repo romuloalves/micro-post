5ab31ce76aba4f5910439c2d71ed634e6b63e545
Rômulo Alves <github@romuloalv.es>
2017-04-25 20:53:38 -0300

    Removing function to get default parameters

--- src/index.js
+++ src/index.js
@@ -2,48 +2,23 @@
 
 const ALLOWED_HTTP_METHOD = 'POST'
 
-const defaultErrorCode = 405
-const defaultResponse = 'Method Not Allowed'
-const defaultContentType = 'text/plain'
-
-function getDefaultParams(params) {
-  if (params == null) {
-    return {
-      errorCode: defaultErrorCode,
-      response: defaultResponse,
-      contentType: defaultContentType
-    }
-  }
-
-  if (params.errorCode == null) {
-    params.errorCode = defaultErrorCode
-  }
-  if (params.response == null) {
-    params.response = defaultResponse
-  }
-  if (params.contentType == null) {
-    params.contentType = defaultContentType
-  }
-
-  return params
-}
-
 module.exports = exports = function (params, fn) {
-  if (typeof params === 'function') {
+  if (!fn) {
     fn = params
-    params = {}
   }
-  params = getDefaultParams(params)
-
   return (req, res) => {
     res.setHeader('Access-Control-Request-Method', ALLOWED_HTTP_METHOD)
     const {method} = req
     if (method !== ALLOWED_HTTP_METHOD) {
-      const response = typeof params.response === 'object' ?
-        JSON.stringify(params.response) :
-        params.response
-      res.writeHead(params.errorCode, {
-        'Content-Type': params.contentType,
+      let {response} = params
+      if (response === null || response === undefined) {
+        response = 'Method Not Allowed'
+      }
+      if (typeof response === 'object') {
+        response = JSON.stringify(response)
+      }
+      res.writeHead((params.errorCode || 405), {
+        'Content-Type': (params.contentType || 'text/plain'),
         'Content-Length': Buffer.byteLength(response)
       })
       res.end(response)
